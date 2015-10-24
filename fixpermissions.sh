c60a09ebfb4a31adeda12d4d057d86ccadd135ee
molotov.dmitry <molotov.dmitry@a6c66fef-f915-412f-8a14-250eefb89e14>
2015-10-24 16:33:57 +0000

    Added script for fixing permissions only

---
+++ fixpermissions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
+cd "${ROOT_PATH}" || exit 1
+
+. "${ROOT_PATH}/functions.sh"
+
+sudo echo -n
+
+clear
+clear
+
+fixpermissions '/media/documents'
+fixpermissions '/media/windows'
