cb8f2348e1ad5bae5404e86161a5158c1c1c055e
molotov.dmitry <molotov.dmitry@a6c66fef-f915-412f-8a14-250eefb89e14>
2016-05-21 22:09:06 +0000

    Changed DLNA server name in server configuration; Added Samba install and config to server configuration; Added Transmission install and config to server configuration.

--- server.sh
+++ server.sh
@@ -22,11 +22,15 @@ else
     exit 1
 fi
 
+### Creating directories for distrib ===========================================
+
+silentsudo 'Creating Distrib directory' mkdir -p /media/documents/Distrib
+silentsudo 'Creating Books directory' mkdir -p /media/documents/Books
+
 ### Fix directory permissions ==================================================
 
 fixpermissions '/media/documents'
 fixpermissions '/media/windows'
-fixpermissions '/media/something'
 
 ### Applications ===============================================================
 
@@ -38,14 +42,16 @@ appupgrade
 ## Install ---------------------------------------------------------------------
 
 appinstall 'MiniDLNA'               'minidlna'
-appinstall 'Transmission'           'transmission-daemon' 
+appinstall 'Transmission'           'transmission-daemon'
+appinstall 'Samba'                  'samba'
+appinstall 'Open SSH'               'openssh-server'
 
 ### Configuration ==============================================================
 
 ## Inotify fix for MiniDLNA ----------------------------------------------------
 
-#silentsudo 'Inotyfy max watchs fix' bash -c 'echo -e "# Increase inotify max watchs per user for local minidlna\nfs.inotify.max_user_watches = 100000" > /etc/sysctl.d/90-inotify.conf'
-#silentsudo 'Inotify max watchs fix' sysctl fs.inotify.max_user_watches=100000
+silentsudo 'Inotyfy max watchs fix' bash -c 'echo -e "fs.inotify.max_user_watches = 100000" > /etc/sysctl.d/90-inotify.conf'
+silentsudo 'Inotify max watchs fix' sysctl fs.inotify.max_user_watches=100000
 
 ## -----------------------------------------------------------------------------
 
@@ -53,15 +59,21 @@ appinstall 'Transmission'           'transmission-daemon'
 
 ## MiniDLNA --------------------------------------------------------------------
 
-#silentsudo 'Stopping MiniDLNA'      service minidlna stop
-#silentsudo 'Copying MiniDLNA config' sudo cp -f "${ROOT_PATH}/files/minidlna/minidlna.conf" "/etc/"
-#silentsudo 'Starting MiniDLNA'      service minidlna start
+silentsudo 'Stopping MiniDLNA'      service minidlna stop
+silentsudo 'Configuring MiniDLNA'   sudo cp -f "${ROOT_PATH}/files/minidlna/minidlna.conf" '/etc/'
+silentsudo 'Starting MiniDLNA'      service minidlna start
 
 ## Transmission ----------------------------------------------------------------
 
-silentsudo 'Stopping Transmission' service transmission-daemon stop
-silentsudo 'Creating dir for Transmission config' sudo mkdir -p "/etc/transmission-daemon"
-silentsudo 'Copying Transmission config' sudo cp -f "${ROOT_PATH}/files/transmission/settings.json" "/etc/transmission-daemon/"
-silentsudo 'Starting Transmission' service transmission-daemon start
+silentsudo 'Stopping Transmission'  service transmission-daemon stop
+silentsudo 'Creating dir for Transmission config' mkdir -p '/etc/transmission-daemon'
+silentsudo 'Configuring Transmission' cp -f "${ROOT_PATH}/files/transmission/settings.json" '/etc/transmission-daemon/'
+silentsudo 'Starting Transmission'  service transmission-daemon start
 
+## Samba -----------------------------------------------------------------------
+
+silentsudo 'Stopping Samba'         service smbd stop
+silentsudo 'Creating dir for Samba config' mkdir -p '/etc/samba'
+silentsudo 'Configuring Samba'      cp -f "${ROOT_PATH}/files/samba/smb.conf" '/etc/samba/'
+silentsudo 'Starting Samba'         service smbd start
 
