c1b7c09a8b5fd031ad850c282f2415d0a6d0e1a9
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2016-06-12 10:13:45 +0000

    Added sftpd config file; Added sftpd install and config to server configuration.

--- custom/tools/server/create.sh
+++ custom/tools/server/create.sh
@@ -36,6 +36,7 @@ appinstall 'Transmission'           'transmission-daemon'
 appinstall 'Samba'                  'samba'
 appinstall 'Open SSH'               'openssh-server'
 appinstall 'EiskaltDC++'            'eiskaltdcpp-daemon'
+appinstall 'FTP server'             'vsftpd'
 
 ### Configuration ==============================================================
 
@@ -50,33 +51,28 @@ silentsudo 'Inotify max watchs fix' sysctl fs.inotify.max_user_watches=100000
 
 ## MiniDLNA --------------------------------------------------------------------
 
-#silentsudo 'Stopping MiniDLNA'      service minidlna stop
-silentsudo 'Configuring MiniDLNA'   sudo cp -f "${ROOT_PATH}/files/minidlna/minidlna.conf" '/etc/'
-#silentsudo 'Starting MiniDLNA'      service minidlna start
+silentsudo 'Configuring MiniDLNA'           sudo cp -f "${ROOT_PATH}/files/minidlna/minidlna.conf" '/etc/'
 
 ## Transmission ----------------------------------------------------------------
 
-#silentsudo 'Stopping Transmission'  service transmission-daemon stop
 silentsudo 'Creating Transmission config dir' mkdir -p '/etc/transmission-daemon'
-silentsudo 'Configuring Transmission' cp -f "${ROOT_PATH}/files/transmission/settings.json" '/etc/transmission-daemon/'
-#silentsudo 'Starting Transmission'  service transmission-daemon start
+silentsudo 'Configuring Transmission'       cp -f "${ROOT_PATH}/files/transmission/settings.json" '/etc/transmission-daemon/'
 
 ## Samba -----------------------------------------------------------------------
 
-#silentsudo 'Stopping Samba'         service smbd stop
-silentsudo 'Creating Samba config dir' mkdir -p '/etc/samba'
-silentsudo 'Configuring Samba'      cp -f "${ROOT_PATH}/files/samba/smb.conf" '/etc/samba/'
-#silentsudo 'Starting Samba'         service smbd start
+silentsudo 'Creating Samba config dir'      mkdir -p '/etc/samba'
+silentsudo 'Configuring Samba'              cp -f "${ROOT_PATH}/files/samba/smb.conf" '/etc/samba/'
 
 ## EiskaltDC++ -----------------------------------------------------------------
 
-#silentsudo 'Stopping EiskaltDC++'           service eiskaltdcpp stop
-#silentsudo 'Disabling EiskaltDC++ service'  systemctl disable eiskaltdcpp
 silentsudo 'Creating EiskaltDC++ config dir' mkdir -p '/etc/eiskaltdcpp'
 silentsudo 'Configuring EiskaltDC++'        cp -f "${ROOT_PATH}/files/eiskaltdcpp/DCPlusPlus.xml" '/etc/eiskaltdcpp/'
 silentsudo 'Configuring EiskaltDC++ Hubs'   cp -f "${ROOT_PATH}/files/eiskaltdcpp/Favorites.xml" '/etc/eiskaltdcpp/'
 silentsudo 'Creating EiskaltDC++ service'   cp -f "${ROOT_PATH}/files/eiskaltdcpp/eiskaltdcpp.service" '/etc/systemd/system/'
 silentsudo 'Enabling Network-online target' mkdir -p /etc/systemd/system/network-online.target.wants
 silentsudo 'Enabling EiskaltDC++ service'   ln -s /etc/systemd/system/eiskaltdcpp.service /etc/systemd/system/network-online.target.wants/eiskaltdcpp.service
-#silentsudo 'Starting EiskaltDC++'           service eiskaltdcpp start
+
+## FTP server ------------------------------------------------------------------
+
+silentsudo 'Configuring vsftpd'             cp -f "${ROOT_PATH}/files/vsftpd/vsfdtpd.conf" '/etc/'
 
