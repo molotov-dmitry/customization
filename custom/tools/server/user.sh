8c2acfbb56fcb3921caa3716da4a6370a0fab3c5
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2016-09-08 12:51:49 +0000

    Fixed svndiff alias for server custom configuration.

--- custom/tools/server/user.sh
+++ custom/tools/server/user.sh
@@ -5,7 +5,7 @@ cd "${ROOT_PATH}" || exit 1
 
 . "${ROOT_PATH}/functions.sh"
 
-echo alias svndiff='svn --diff-cmd "colordiff" --extensions "-y -W $(( $(tput cols) - 2 ))" diff' >> ~/.bash_aliases
+echo alias svndiff=\'svn --diff-cmd "colordiff" --extensions '"-y -W $(( $(tput cols) - 2 ))"' diff\' >> ~/.bash_aliases
 
 clear
 clear
