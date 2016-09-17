17f6f1fb3481bfc2edcf7dfc6e0f2f71f829f522
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2016-09-17 12:24:02 +0000

    Added FTP server configuration for work configuration.

--- custom/tools/work/prepare.sh
+++ custom/tools/work/prepare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo cp -rf "${ROOT_PATH}/files/vsftpd"          "${rootfs_dir}/tools/files/"
 sudo cp -rf "${ROOT_PATH}/files/libreoffice" "${rootfs_dir}/tools/files/"
 
 sudo mkdir -p "${rootfs_dir}/etc/network/interfaces.d"
