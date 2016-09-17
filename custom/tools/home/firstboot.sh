590b061f161cfb729200e7446112a4bcc819a157
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2016-09-17 21:19:09 +0000

    Added home custom configuration; Added gsettings array functions; Added gnome shell extension installation; Added media player gnome shell extension for work and home custom configurations; Changed monospace font size to 12 for work and home custom configurations; Removed libqt5libqgtk2 usage for DE other than Gnome for work and home custom configurations;

--- custom/tools/home/firstboot.sh
+++ custom/tools/home/firstboot.sh
@@ -25,15 +25,6 @@ then
     echo 'export QT_QPA_PLATFORMTHEME=qt5gtk2' > /etc/X11/Xsession.d/100-qt5gtk2
 fi
 
-### Add samba shares to fstab ==================================================
-
-sed -i '/[ \t]cifs[ \t]/d' /etc/fstab
-
-echo '' >> /etc/fstab
-echo '//172.16.8.91/usr /media/dima cifs guest,uid=1000,forceuid,gid=1000,forcegid,file_mode=0775,dir_mode=0775,iocharset=utf8 0 0' >> /etc/fstab
-echo '//172.16.8.91/share2 /media/cub_local cifs guest,uid=1000,forceuid,gid=1000,forcegid,file_mode=0775,dir_mode=0775,iocharset=utf8 0 0' >> /etc/fstab
-echo '//172.16.8.21/share2 /media/cub cifs guest,uid=1000,forceuid,gid=1000,forcegid,file_mode=0775,dir_mode=0775,iocharset=utf8 0 0' >> /etc/fstab
-
 ### Fix directory permissions ==================================================
 
 fixpermissions '/media/documents'
