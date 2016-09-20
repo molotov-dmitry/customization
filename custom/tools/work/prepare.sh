e816fa40c1a7223952fedddf6dc444ebc37e5bcd
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2016-09-20 10:12:14 +0000

    Removed system network configuration from work custom configuration.

--- custom/tools/work/prepare.sh
+++ custom/tools/work/prepare.sh
@@ -14,7 +14,3 @@ done
 silentsudo "Copy vsftpd config" cp -rf "${ROOT_PATH}/files/vsftpd"          "${rootfs_dir}/tools/files/"
 silentsudo "Copy Libreoffice config" cp -rf "${ROOT_PATH}/files/libreoffice" "${rootfs_dir}/tools/files/"
 
-## Network interface config ----------------------------------------------------
-
-sudo mkdir -p "${rootfs_dir}/etc/network/interfaces.d"
-silentsudo "Copy network interface config" cp -rf "${custom_file_path}/eth0.interface" "${rootfs_dir}/etc/network/interfaces.d/"
