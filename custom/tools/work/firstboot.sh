b7c1826e29a4815a1002c2755efec9ab6d3ee274
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2016-09-29 18:12:55 +0000

    Added bundles for work custom configuration.

--- custom/tools/work/firstboot.sh
+++ custom/tools/work/firstboot.sh
@@ -10,21 +10,6 @@ sudo echo -n
 clear
 clear
 
-### Customization ==============================================================
-
-## Cursor theme ----------------------------------------------------------------
-
-cursor_theme='breeze_cursors'
-
-update-alternatives --set x-cursor-theme "/etc/X11/cursors/${cursor_theme}.theme"
-
-## Qt5 GTK2 theme --------------------------------------------------------------
-
-if ispkginstalled gnome-shell
-then
-    echo 'export QT_QPA_PLATFORMTHEME=qt5gtk2' > /etc/X11/Xsession.d/100-qt5gtk2
-fi
-
 ### Add samba shares to fstab ==================================================
 
 sed -i '/[ \t]cifs[ \t]/d' /etc/fstab
