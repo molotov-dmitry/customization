2525819a3c0137a328ce51e44682210c44d644e1
molotov.dmitry <molotov.dmitry@a6c66fef-f915-412f-8a14-250eefb89e14>
2016-11-04 18:16:28 +0000

    Updated EiskaltDC++ Remote package to version 24.

--- bundles/install.sh
+++ bundles/install.sh
@@ -327,7 +327,7 @@ case "${bundle}" in
 "network-remote")
 
     appinstall 'Transmission remote'    'transmission-remote-gtk'
-    debinstall 'EiskaltDC++ Remote Qt'  'eiskaltdcpp-remote-qt' '22' 'amd64'
+    debinstall 'EiskaltDC++ Remote Qt'  'eiskaltdcpp-remote-qt' '24' 'amd64'
 
 ;;
 
--- bundles/prepare.sh
+++ bundles/prepare.sh
@@ -98,7 +98,7 @@ case "${bundle}" in
 
     silentsudo 'Copy Transmission remote config' cp -rf "${ROOT_PATH}/files/transmission-remote-gtk" "${rootfs_dir}/tools/files/"
 
-    debprepare 'EiskaltDC++ Remote Qt' 'eiskaltdcpp-remote-qt' '22' 'amd64'
+    debprepare 'EiskaltDC++ Remote Qt' 'eiskaltdcpp-remote-qt' '24' 'amd64'
 ;;
 
 ### ============================================================================
