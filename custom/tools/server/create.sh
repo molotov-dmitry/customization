f46217b38a5b90f3d2fd2fcca7e514d6e9c0ffde
molotov.dmitry <molotov.dmitry@a6c66fef-f915-412f-8a14-250eefb89e14>
2017-01-04 19:07:48 +0000

    Changed server custom script for using bundles.

--- custom/tools/server/create.sh
+++ custom/tools/server/create.sh
@@ -23,64 +23,17 @@ fi
 
 ### Applications ===============================================================
 
-## Updating --------------------------------------------------------------------
+## Update ----------------------------------------------------------------------
 
 appupdate
 appupgrade
 
 ## Install ---------------------------------------------------------------------
 
-appinstall 'MiniDLNA'               'minidlna'
-appinstall 'Transmission'           'transmission-daemon'
-appinstall 'Samba'                  'samba'
-appinstall 'Open SSH'               'openssh-server'
-appinstall 'EiskaltDC++'            'eiskaltdcpp-daemon'
-appinstall 'FTP server'             'vsftpd'
-appinstall 'Iperf'                  'iperf iperf3'
+bundle install 'dev/build'
+bundle install 'dev/db'
 
-appinstall 'VCS'                    'git subversion colordiff'
+bundle install 'server'
 
-appinstall 'Tree'                   'tree'
-appinstall 'Midnight Commander'     'mc'
-
-### Configuration ==============================================================
-
-## Inotify fix for MiniDLNA ----------------------------------------------------
-
-silentsudo 'Inotyfy max watchs fix' bash -c 'echo -e "fs.inotify.max_user_watches = 100000" > /etc/sysctl.d/90-inotify.conf'
-silentsudo 'Inotify max watchs fix' sysctl fs.inotify.max_user_watches=100000
-
-## -----------------------------------------------------------------------------
-
-### Application configuration ==================================================
-
-## MiniDLNA --------------------------------------------------------------------
-
-silentsudo 'Configuring MiniDLNA'           sudo cp -f "${ROOT_PATH}/files/minidlna/minidlna.conf" '/etc/'
-
-## Transmission ----------------------------------------------------------------
-
-silentsudo 'Creating Transmission config dir' mkdir -p '/etc/transmission-daemon'
-silentsudo 'Configuring Transmission'       cp -f "${ROOT_PATH}/files/transmission/settings.json" '/etc/transmission-daemon/'
-
-## Samba -----------------------------------------------------------------------
-
-silentsudo 'Creating Samba config dir'      mkdir -p '/etc/samba'
-silentsudo 'Configuring Samba'              cp -f "${ROOT_PATH}/files/samba/smb.conf" '/etc/samba/'
-
-## EiskaltDC++ -----------------------------------------------------------------
-
-silentsudo 'Creating EiskaltDC++ config dir' mkdir -p '/etc/eiskaltdcpp'
-silentsudo 'Configuring EiskaltDC++'        cp -f "${ROOT_PATH}/files/eiskaltdcpp/DCPlusPlus.xml" '/etc/eiskaltdcpp/'
-silentsudo 'Configuring EiskaltDC++ Hubs'   cp -f "${ROOT_PATH}/files/eiskaltdcpp/Favorites.xml" '/etc/eiskaltdcpp/'
-
-addservice 'EiskaltDC++' 'eiskaltdcpp' 'eiskaltdcpp'
-
-#silentsudo 'Creating EiskaltDC++ service'   cp -f "${ROOT_PATH}/files/eiskaltdcpp/eiskaltdcpp.service" '/etc/systemd/system/'
-#silentsudo 'Enabling Network-online target' mkdir -p /etc/systemd/system/network-online.target.wants
-#silentsudo 'Enabling EiskaltDC++ service'   ln -s /etc/systemd/system/eiskaltdcpp.service /etc/systemd/system/network-online.target.wants/eiskaltdcpp.service
-
-## FTP server ------------------------------------------------------------------
-
-silentsudo 'Configuring vsftpd'             cp -f "${ROOT_PATH}/files/vsftpd/vsftpd.conf" '/etc/'
+bundle install 'cli'
 
--- custom/tools/server/firstboot.sh
+++ custom/tools/server/firstboot.sh
@@ -5,30 +5,18 @@ cd "${ROOT_PATH}" || exit 1
 
 . "${ROOT_PATH}/functions.sh"
 
-sudo echo -n
-
-clear
-clear
-
-### Test internet connection ===================================================
-
-title 'Testing internet connection'
-
-if conntest
-then
-    msgdone
-else
-    msgfail
-    exit 1
-fi
-
 ### Creating directories for distrib ===========================================
 
+silentsudo 'Creating Videos directory'      mkdir -p /media/documents/Video     -m 0777
+silentsudo 'Creating Music directory'       mkdir -p /media/documents/Music     -m 0777
+silentsudo 'Creating Images directory'      mkdir -p /media/documents/Images    -m 0777
 silentsudo 'Creating Distrib directory'     mkdir -p /media/documents/Distrib   -m 0777
+silentsudo 'Creating Documents directory'   mkdir -p /media/documents/Documents -m 0777
 silentsudo 'Creating Books directory'       mkdir -p /media/documents/Books     -m 0777
 silentsudo 'Creating Archive directory'     mkdir -p /media/documents/Archive   -m 0777
 silentsudo 'Creating incomplete direcrory'  mkdir -p /media/documents/Downloads/Incomplete -m 0777
 
 ### Fix directory permissions ==================================================
 
-fixpermissions '/media/documents'
+fixpermissions '/media/documents' 1000
+
