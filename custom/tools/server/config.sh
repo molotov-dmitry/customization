a4bbed12b909c7cb92091cb8cdc65b198d61e881
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2017-01-04 19:09:33 +0000

    Fixed missing config script in server configuration.

---
+++ custom/tools/server/config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
+cd "${ROOT_PATH}" || exit 1
+
+. "${ROOT_PATH}/functions.sh"
+
+sudo echo -n
+
+clear
+
+### Bundles ====================================================================
+
+bundle config 'dev/build'
+
+bundle config 'server'
+
