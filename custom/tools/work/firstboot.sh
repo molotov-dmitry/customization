111bc46a198cbacc0bd6d44756b32cd2dc347ac0
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2017-06-27 18:47:10 +0000

    Added wireshark fix to 'dev/net' firstboot bundle.

--- custom/tools/work/firstboot.sh
+++ custom/tools/work/firstboot.sh
@@ -5,16 +5,7 @@ cd "${ROOT_PATH}" || exit 1
 
 . "${ROOT_PATH}/functions.sh"
 
-sudo echo -n
+### Development ================================================================
 
-clear
-clear
+bundle firstboot 'dev'
 
-### Add all users to wireshark group ===========================================
-
-for userinfo in $(cat /etc/passwd | grep -v nologin | grep -v /bin/false | grep -v /bin/sync | grep -v postgres | grep -v ftp | cut -d ':' -f 1,6)
-do
-    user_name=$(echo "${userinfo}" | cut -d ':' -f 1)
-
-    usermod -a -G wireshark ${user_name}
-done
