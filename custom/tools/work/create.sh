926d996e78f1316a249ce8f74b5cd72440732de5
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2018-04-07 12:33:32 +0000

    Added remote desktop client and server applications install to work configuration; Added domain hosts to work configuration.

--- custom/tools/work/create.sh
+++ custom/tools/work/create.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
+cd "${ROOT_PATH}" || exit 1
+
+. "${ROOT_PATH}/functions.sh"
+
+### ============================================================================
+
+appinstall 'RDP server' 'vino'
+appinstall 'RDP client' 'remmina remmina-plugin-vnc remmina-plugin-rdp'
