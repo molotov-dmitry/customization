2e4116de914d57a905c4e1f48dde43e12e89dcb7
molotov.dmitry <molotov.dmitry@a6c66fef-f915-412f-8a14-250eefb89e14>
2018-04-07 14:26:32 +0000

    Updated work server configuratio

--- custom/tools/server_work/prepare.sh
+++ custom/tools/server_work/prepare.sh
@@ -2,20 +2,5 @@
 
 ### Copy config files ==========================================================
 
-## Service unit files ----------------------------------------------------------
-
-for srv in irbuild.service irserver.service timesync.service
-do
-    silentsudo "Copy ${srv//.*} unit" cp -f "${custom_file_path}/${srv}" "${rootfs_dir}/tools/files/"
-done
-
-## Application config files ----------------------------------------------------
-
-bundle prepare 'server/ftp'
-bundle prepare 'server/smb'
-bundle prepare 'server/svn'
-
-## Network configuration -------------------------------------------------------
-
 sudo mkdir -p "${rootfs_dir}/etc/network/interfaces.d"
 sudo cp -rf "${custom_file_path}/eth0.interface" "${rootfs_dir}/etc/network/interfaces.d/"
--- custom/tools/server_work/user.sh
+++ custom/tools/server_work/user.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 
-ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
-cd "${ROOT_PATH}" || exit 1
-
-. "${ROOT_PATH}/functions.sh"
-
-bundle user 'vcs'
-bundle user 'cli'
-
