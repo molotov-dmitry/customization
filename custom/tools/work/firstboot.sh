08507f3b0ea20f48b3497d6e9be72acab58c6931
molotov.dmitry <molotov.dmitry@a6c66fef-f915-412f-8a14-250eefb89e14>
2018-04-07 12:33:32 +0000

    Added remote desktop client and server applications install to work configuration; Added domain hosts to work configuration.

--- custom/tools/work/firstboot.sh
+++ custom/tools/work/firstboot.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+cat << EOF >> /etc/hosts
+172.16.56.14    rczifort.local
+172.16.56.15    redmine.rczifort.local
+172.16.56.16    chat.rczifort.local
+172.16.56.17    ex01.rczifort.local
+172.16.56.22    git.rczifort.local
+172.16.56.23    data.rczifort.local
+
+EOF
+
