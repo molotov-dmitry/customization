17f4f642e571b200de5b56f5144963f1bbb61bf4
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2018-05-11 05:53:58 +0000

    Added Pidgin launcher; Added Evolution launcher; Added AHome SFTP bookmark in work configuration.

--- custom/tools/work/user.sh
+++ custom/tools/work/user.sh
@@ -11,6 +11,7 @@ cd "${ROOT_PATH}" || exit 1
 
 mkdir -p "${HOME}/.config/gtk-3.0/"
 
+addbookmark 'sftp://188.134.72.31:2222' 'AHOME'
 addbookmark 'smb://172.16.8.21/share2'  'KUB'
 addbookmark 'smb://172.16.8.203'        'NAS'
 addbookmark 'smb://data.rczifort.local' 'DATA'
