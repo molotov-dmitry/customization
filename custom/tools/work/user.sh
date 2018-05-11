8e002e6d4c94711db0f503c8af9eb605de19004c
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2018-05-11 06:28:45 +0000

    Updated AHome bookmark in work configuration.

--- custom/tools/work/user.sh
+++ custom/tools/work/user.sh
@@ -11,7 +11,8 @@ cd "${ROOT_PATH}" || exit 1
 
 mkdir -p "${HOME}/.config/gtk-3.0/"
 
-addbookmark 'sftp://188.134.72.31:2222' 'AHOME'
+addbookmark 'sftp://188.134.72.31:2222/media/documents' 'AHOME'
+
 addbookmark 'smb://172.16.8.21/share2'  'KUB'
 addbookmark 'smb://172.16.8.203'        'NAS'
 addbookmark 'smb://data.rczifort.local' 'DATA'
