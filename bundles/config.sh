4b7058b2b84ddd949c9524c08bba86b7f82d22e0
molotov.dmitry <molotov.dmitry@a6c66fef-f915-412f-8a14-250eefb89e14>
2018-05-11 17:13:50 +0000

    Added Grub theme configuration.

--- bundles/config.sh
+++ bundles/config.sh
@@ -348,6 +348,10 @@ case "${bundle}" in
 
 "appearance/themes")
 
+    ## Grub theme --------------------------------------------------------------
+
+    silent 'Set Grub theme' sed -i 's/44,0,30,0/55,71,79,0/' /usr/share/plymouth/themes/default.grub
+
     ## Cursor theme ------------------------------------------------------------
 
     cursor_theme='breeze_cursors'
