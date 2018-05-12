9ad13b6a26d8d08a6bcef1fea75c1f439a4f7f99
molotov.dmitry <molotov.dmitry@a6c66fef-f915-412f-8a14-250eefb89e14>
2018-05-12 18:36:32 +0000

    Added libs and qmake to Qt4 bundle.

--- bundles/install.sh
+++ bundles/install.sh
@@ -318,7 +318,8 @@ case "${bundle}" in
 
 "dev/qt4")
 
-    appinstall 'Qt4 SDK'                'qt4-dev-tools libqt4-qt3support'
+    appinstall 'Qt4 SDK'                'qt4-dev-toolsqt4-qmake'
+    appinstall 'Qt4 Libs'               'libqt4-dev libqt4-dev-bin libqt4-network libqt4-sql libqtcore4 libqt4-qt3support'
 
 ;;
 
