636a5f6e9e8c90df1bf00a550725bbe430f2cbae
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2018-09-04 16:41:30 +0000

    Added DLNA support packages to media bundle.

--- bundles/install.sh
+++ bundles/install.sh
@@ -459,6 +459,8 @@ case "${bundle}" in
     appinstall 'Totem video player'     'totem'
     appinstall 'MPV Player'             'mpv gnome-mpv'
 
+    appinstall 'DLNA support'           'dleyna-server dleyna-renderer'
+
     appinstall 'EasyTag'                'easytag'
 
     appinstall 'Gnome Photos'           'gnome-photos'
