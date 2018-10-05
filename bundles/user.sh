fb73c9752192769e1c3876c4deae1f066d0900aa
molotov.dmitry <molotov.dmitry@a6c66fef-f915-412f-8a14-250eefb89e14>
2018-10-05 19:23:23 +0000

    Updated compare Nautilus scenario.

--- bundles/user.sh
+++ bundles/user.sh
@@ -500,7 +500,7 @@ case "${bundle}" in
 
     if ispkginstalled 'meld' && ispkginstalled 'nautilus'
     then
-        addscenario 'compare' 'F3' 'svn info && meld .'
+        addscenario 'compare' 'F3' '[[ $# -eq 0 ]] && svn info && meld .\n[[ $# -eq 1 ]] && svn info "$1" && meld "$1"\n[[ $# -gt 1 ]] && meld $@'
     fi
 
 ;;
