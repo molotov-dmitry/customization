80da3a7f5ce9a4401ee9dac6e777621b452d9bcd
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2018-10-06 15:12:12 +0000

    Updated Q190 customization scripts.

--- custom/tools/q190/create.sh
+++ custom/tools/q190/create.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
+cd "${ROOT_PATH}" || exit 1
 
+. "${ROOT_PATH}/functions.sh"
+
+### ============================================================================
+
+appinstall 'RDP server' 'vino'
+
+if [[ "$(lsb_release -si) $(lsb_release -sc)" == 'Debian stretch' ]]
+then
+    appinstall 'RDP client' 'vinagre'
+else
+    appinstall 'RDP client' 'remmina remmina-plugin-vnc remmina-plugin-rdp'
+fi
