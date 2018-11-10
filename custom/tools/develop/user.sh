8bee50506bc0b967a7dadc54daf0c297424b43af
Дмитрий Сорокин <molotov.dmitry@gmail.com>
2018-11-10 20:21:34 +0300

    Added develop configuration.

---
+++ custom/tools/develop/user.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
+cd "${ROOT_PATH}" || exit 1
+
+. "${ROOT_PATH}/functions.sh"
+
+### User network configuration =================================================
+
+### Add network shares ---------------------------------------------------------
+
+mkdir -p "${HOME}/.config/gtk-3.0/"
+
+addbookmark 'sftp://188.134.72.31:2222/media/documents' 'AHOME'
+
+addbookmark 'smb://172.16.8.21/share2'  'KUB'
+addbookmark 'smb://172.16.8.203'        'NAS'
+addbookmark 'smb://data.rczifort.local' 'DATA'
+
+### Customization ==============================================================
+
+## Make GIT accept self-signed certificate -------------------------------------
+
+git config --global http.sslVerify false
+
+## Clear launcher --------------------------------------------------------------
+
+launcherclear
+
+## Appearance ------------------------------------------------------------------
+
+setwallpaper '#204a87'
+
