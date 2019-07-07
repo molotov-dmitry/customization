03cfc5b032bc60ba43f144dfe08dfc672946d6d7
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-07-07 15:02:24 +0300

    Updated UTF-8 BOM function.

--- bundles/user.sh
+++ bundles/user.sh
@@ -482,12 +482,20 @@ for file in "$@"
 do
     if test -s "${file}"
     then
-        [[ $(cut -z -b1-3 "${file}" | hexdump -v -e '/1 "%02X"') == 'EFBBBF00' ]] || sed -i '1s/^/\xef\xbb\xbf/' "${file}" || exit $?
+        if [[ $(cut -z -b1-3 "${file}" | hexdump -v -e '/1 "%02X"') == 'EFBBBF00' ]]
+        then
+            echo "Unchanged ${file}"
+        else
+            sed -i '1s/^/\xef\xbb\xbf/' "${file}" || return $?
+            echo "Added UTF-8 BOM to ${file}"
+        fi
     elif test -w $file
     then
-        echo -en "\xef\xbb\xbf" > "${file}"
+        echo -en "\xef\xbb\xbf" > "${file}" || return $?
+        echo "Added UTF-8 BOM to ${file}"
     else
-        exit 1
+        file "${file}" || return $?
+        return 1
     fi
 done
 }
