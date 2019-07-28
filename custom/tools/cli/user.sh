53393f5268594e161f6392e5b41cb4e36b1f83f9
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-07-28 21:36:29 +0300

    Added wpaconnect() function to CLI configuration.

--- custom/tools/cli/user.sh
+++ custom/tools/cli/user.sh
@@ -1,2 +1,46 @@
 #!/bin/bash
 
+cat >> ~/.bash_aliases << '_EOF'
+function wpaconnect()
+{
+    local essid="$1"
+
+    if [[ -z "$2" ]]
+    then
+        local iface="wlan0"
+    else
+        local iface="$2"
+    fi
+
+    while [[ ! -f "${HOME}/.local/share/wpa_essid/${essid}" ]]
+    do
+        mkdir -p "${HOME}/.local/share/wpa_essid"
+
+        echo "input password:"
+
+        wpa_passphrase ${essid} > "${HOME}/.local/share/wpa_essid/${essid}"
+    done
+
+    sudo wpa_supplicant -B -i $iface -c "${HOME}/.local/share/wpa_essid/${essid}"
+
+    local result=$?
+    if [[ $result -ne 0 ]]
+    then
+        return $result
+    fi
+
+    sudo timeout 5 dhclient $iface
+
+    local result=$?
+    if [[ $result -ne 0 ]]
+    then
+        echo 'Not connected'
+
+        return $result
+    fi
+
+    echo 'Connected'
+
+    return 0
+}
+_EOF
