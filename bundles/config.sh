daf1ef961459e34dd2617c800d6a6e06afbbcb05
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-07-28 21:35:40 +0300

    Fixed adding Code Generation Tools bin directory to PATH.

--- bundles/config.sh
+++ bundles/config.sh
@@ -366,7 +366,7 @@ case "${bundle}" in
 
 "dev/ti")
 
-    sed -i 's/PATH="\([^"]*\)"/PATH="\1:\/opt\/TI\/C6000CGT6.0.11\/bin"/' /etc/environment
+    echo 'PATH="${PATH}:/opt/TI/C6000CGT6.0.11/bin"'         >> /etc/environment
     echo 'export C6X_C_DIR="/opt/TI/C6000CGT6.0.11/include"' >> /etc/environment
 
     silent 'Update MIME info'       update-mime-database /usr/local/share/mime
