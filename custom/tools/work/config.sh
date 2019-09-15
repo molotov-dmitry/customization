8fd8bbeb3db80df8871de609838b4ea1043344a5
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-09-15 13:23:30 +0300

    Added NetworkManager configuration for 8812AU driver.

--- custom/tools/work/config.sh
+++ custom/tools/work/config.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 
+#### Configure NetworkManager for using Realtek 8812AU driver ==================
+
+addconfigline 'wifi.scan-rand-mac-address' 'no' 'device' '/etc/NetworkManager/NetworkManager.conf'
