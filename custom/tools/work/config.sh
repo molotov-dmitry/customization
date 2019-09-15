af6293c1f6a52888e4cdde27694bdd00aee991ea
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-09-15 15:51:21 +0300

    Fixed work configuration.

--- custom/tools/work/config.sh
+++ custom/tools/work/config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
+cd "${ROOT_PATH}" || exit 1
+
+. "${ROOT_PATH}/functions.sh"
+
 #### Configure NetworkManager for using Realtek 8812AU driver ==================
 
 addconfigline 'wifi.scan-rand-mac-address' 'no' 'device' '/etc/NetworkManager/NetworkManager.conf'
