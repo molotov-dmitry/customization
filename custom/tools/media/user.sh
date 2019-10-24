6473f898160f1635fbd3feeefa49b92e6a017060
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-10-24 11:02:46 +0300

    Added media and test bundles.

---
+++ custom/tools/media/user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
+cd "${ROOT_PATH}" || exit 1
+
+. "${ROOT_PATH}/functions.sh"
+
+### Customization ==============================================================
+
+## Clear launcher --------------------------------------------------------------
+
+launcherclear
+
