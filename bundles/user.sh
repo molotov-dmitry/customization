7ca38b72aca225e897f1ce6aeef34e02f72c3483
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-12-27 10:03:37 +0300

    Added settings RCZI network printer as default printer.

--- bundles/user.sh
+++ bundles/user.sh
@@ -1444,6 +1444,8 @@ _EOF
             lpadmin -p 'HP_Laserjet_1320' -L 'Комната 8' -E \
                 -v 'socket://172.16.8.200:9100' \
                 -m 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/Generic-PCL_5e_Printer-hpijs-pcl5e.ppd'
+
+            lpadmin -d 'HP_Laserjet_1320'
         fi
     fi
 
