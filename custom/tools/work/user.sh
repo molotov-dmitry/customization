d9feea76622fbfa5d1489fc3654c8ebf868e784c
Dmitry Sorokin <molotov.dmitry@gmail.com>
2020-01-13 12:48:07 +0300

    Added network-switch autostart to work build configuration.

--- custom/tools/work/user.sh
+++ custom/tools/work/user.sh
@@ -10,6 +10,28 @@ cd "${ROOT_PATH}" || exit 1
 mkdir -p "${HOME}/.config"
 touch    "${HOME}/.config/is-work-account"
 
+### Add network switch =========================================================
+
+if ispkginstalled network-switch
+then
+    nettype=eth
+
+    mkdir -p "${HOME}/.config/autostart"
+
+    cat > "${HOME}/.config/autostart/network-switch.desktop" << _EOF
+[Desktop Entry]
+Version=1.0
+Name=Network switcher
+Comment=Network switcher
+Exec=network-switch ${nettype}
+Terminal=false
+Type=Application
+Categories=Network
+_EOF
+
+    unset nettype
+fi
+
 ## Clear launcher ==============================================================
 
 launcherclear
