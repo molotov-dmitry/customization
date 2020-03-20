f17c0f139712ea0120abae8963375d18bcfce192
Dmitry Sorokin <molotov.dmitry@gmail.com>
2020-03-20 16:51:16 +0300

    Added networking configuration to work server build configuration.

--- custom/tools/server_work/firstboot.sh
+++ custom/tools/server_work/firstboot.sh
@@ -5,6 +5,25 @@ cd "${ROOT_PATH}" || exit 1
 
 . "${ROOT_PATH}/functions.sh"
 
+### Add nameserver configuration ===============================================
+
+for dns in '172.16.56.14' '172.16.56.10'
+do
+    if [[ -z "$(grep "${dns}$" /etc/resolv.conf)" ]]
+    then
+        echo "nameserver $dns" >> /etc/resolv.conf
+    fi
+done
+
+if [[ -z "$(grep "rczifort.local$" /etc/resolv.conf)" ]]
+then
+    echo "domain rczifort.local" >> /etc/resolv.conf
+fi
+
+### Remove setup network configuration =========================================
+
+rm -f '/etc/network/interfaces.d/setup'
+
 ### Add samba shares to fstab ==================================================
 
 sed -i '/[ \t]/media/backup[ \t]/d' /etc/fstab
