5ddfdf7e89ce13d83a4216e55a4aef71576f83cc
Dmitry Sorokin <molotov.dmitry@gmail.com>
2020-04-19 21:50:31 +0300

    Added updating icon cache for Qt Designer.

--- bundles/config.sh
+++ bundles/config.sh
@@ -367,6 +367,7 @@ _EOF
 
     silent 'Diverting Qt MIME'      dpkg-divert --add --rename --divert /usr/share/mime/packages/Nokia-QtCreator.xml.bak /usr/share/mime/packages/Nokia-QtCreator.xml
     silent 'Updating MIME database' update-mime-database /usr/share/mime
+    silent 'Updating icon cache'    update-icon-caches /usr/share/icons/*
 
 ;;
 
