d045e33849eef6b4df46ef9f00e4e02596de086a
Dmitry Sorokin <molotov.dmitry@gmail.com>
2020-04-25 20:42:43 +0300

    Added UCM files for Intel platforms package to tablet build configuration.

--- custom/tools/tablet/create.sh
+++ custom/tools/tablet/create.sh
@@ -5,56 +5,8 @@ cd "${ROOT_PATH}" || exit 1
 
 . "${ROOT_PATH}/functions.sh"
 
-### Test internet connection ===================================================
-
-title 'Testing internet connection'
-
-if conntest
-then
-    msgdone
-else
-    msgfail
-    exit 1
-fi
-
-### Applications ===============================================================
-
-appinstall 'Git' 'git'
-
-## Modem -----------------------------------------------------------------------
-
-appinstall 'Modem tools' 'libmbim-utils libqmi-utils minicom'
-
 ### Drivers ====================================================================
-
-silent 'Creating directory for drivers' mkdir -p /usr/bin/drivers
-
-## Kernel headers --------------------------------------------------------------
-
-bundle install 'dev/build'
-
-for kernelver in $(kernelversionlist)
-do
-    appinstall "${kernelver} kernel headers"     "linux-headers-${kernelver}"
-done
-
-if [[ "$(lsb_release -si)" == "Ubuntu" ]]
-then
-    appinstall 'Generic kernel headers'     'linux-headers-generic'
-fi
 
 ## Audio -----------------------------------------------------------------------
 
-cd /usr/bin/drivers
-
-silent 'Cloning audio configs'      git clone --depth 1 https://github.com/plbossart/UCM.git
-
-cd UCM
-
-silent 'Installing configs'         cp -rf ./ /usr/share/alsa/ucm/
-
-silent 'Blacklist audio module'     sh -c "echo 'blacklist snd_hdmi_lpe_audio' > /etc/modprobe.d/blacklist-snd-hdmi-lpe-audio.conf"
-
-## Bluetooth -------------------------------------------------------------------
-
-gitinstall 'Bluetooth driver'       'https://github.com/lwfinger/rtl8723bs_bt.git' make
+appinstall 'Intel UCM files' 'ucm-intel'
