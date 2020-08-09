feb1f3438cd695738594881c0f8b328f2f7fac40
Dmitry Sorokin <molotov.dmitry@gmail.com>
2020-08-09 19:58:08 +0300

    Added RTL8192EU driver to work build configuration.

--- custom/tools/work/create.sh
+++ custom/tools/work/create.sh
@@ -12,6 +12,7 @@ appinstall 'Xorriso'                'xorriso'
 ### Wi-Fi driver ===============================================================
 
 appinstall 'RTL8812AU driver'       'rtl8812au-dkms'
+appinstall 'RTL8192EU driver'       'rtl8192eu-config rtl8192eu-dkms'
 
 ### Report builder =========================================================
 
