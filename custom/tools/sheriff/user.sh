300ff2063a40bb2f3317258d3e014299313455cb
Dmitry Sorokin <molotov.dmitry@gmail.com>
2020-12-06 19:55:34 +0300

    Added SheriffFox build configuration.

---
+++ custom/tools/sheriff/user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
+cd "${ROOT_PATH}" || exit 1
+
+. "${ROOT_PATH}/functions.sh"
+
+## Set any user as work account ================================================
+
+mkdir -p "${HOME}/.config"
+touch    "${HOME}/.config/is-work-account"
+
+## Clear launcher --------------------------------------------------------------
+
+launcherclear
+
+
