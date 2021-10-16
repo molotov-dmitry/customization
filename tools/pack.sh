8e1a0881e39854b7e09450a14aa6620c3480df8d
Dmitry Sorokin <molotov.dmitry@gmail.com>
2021-10-16 19:45:41 +0300

    Updated MD5 and revisions list generation.

--- tools/pack.sh
+++ tools/pack.sh
@@ -61,19 +61,20 @@ function packiso()
         silent 'Making iso hybrid' isohybrid "${res_dir}/${iso_name}" || exit 1
     fi
 
-    if grep -sq "[ /]${iso_name}\$" "${res_dir}/MD5SUMS"
-    then
-        silent 'Removing old iso md5' sed -i "/[ /]${iso_name}\$/d" "${res_dir}/MD5SUMS"
-    fi
+    sed -i "/[ /]${iso_name}\$/d" "${res_dir}/MD5SUMS" 2>/dev/null
+    md5sum "${res_dir}/${iso_name}" | sed 's/\/.*\///' >> "${res_dir}/MD5SUMS"
 
-    silent 'Generating md5 for iso' bash -c "md5sum \"${res_dir}/${iso_name}\" | sed 's/\/.*\///' >> \"${res_dir}/MD5SUMS\""
+    sed -i "/[ /]${iso_name}\$/d" "${res_dir}/REV" 2>/dev/null
+    echo "$(git log --pretty=format:'%h' | wc -w) $(git rev-parse --short HEAD) ${iso_name}" >> "${res_dir}/REV"
 
-    if grep -sq "[ /]${iso_name}\$" "${res_dir}/REV"
-    then
-        silent 'Removing old iso revision' sed -i "/[ /]${iso_name}\$/d" "${res_dir}/REV"
-    fi
-
-    silent 'Generating revision for iso' bash -c "echo \"$(git log --pretty=format:'%h' | wc -w) $(git rev-parse --short HEAD) ${iso_name}\" >> \"${res_dir}/REV\""
+    for iso in $(cut -d ' ' -f 3 "${res_dir}/MD5SUMS" "${res_dir}/REV" | sort -u)
+    do
+        if [[ ! -f "${res_dir}/${iso}" ]]
+        then
+            sed -i "/[ /]${iso}\$/d" "${res_dir}/MD5SUMS" 2>/dev/null
+            sed -i "/[ /]${iso}\$/d" "${res_dir}/REV"     2>/dev/null
+        fi
+    done
 
     silent 'Changing rights for iso' chmod -R a+rw "${res_dir}"
 }
