aee8b60db366077715dfc0cabd66ce825f120496
Dmitry Sorokin <molotov.dmitry@gmail.com>
2022-05-09 19:46:42 +0300

    Removed GSConnect extension.

--- custom/tools/home/install.sh
+++ custom/tools/home/install.sh
@@ -6,5 +6,3 @@ cd "${ROOT_PATH}" || exit 1
 . "${ROOT_PATH}/functions.sh"
 
 appinstall 'Notify server' 'notify-server'
-
-gnomeshellextension 1319 'GSConnect' '' 'gnome-shell-extension-gsconnect'
--- custom/tools/home/user.sh
+++ custom/tools/home/user.sh
@@ -7,13 +7,6 @@ cd "${ROOT_PATH}" || exit 1
 
 ### Customization ==============================================================
 
-## GSConnect -------------------------------------------------------------------
-
-if ispkginstalled gnome-shell
-then
-    gsettingsadd org.gnome.shell enabled-extensions 'gsconnect@andyholmes.github.io'
-fi
-
 ## Clear launcher --------------------------------------------------------------
 
 launcherclear
