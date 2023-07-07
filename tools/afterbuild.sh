db9a3b40e45c1eb09d7b87c770fcebdcada896eb
Dmitry Sorokin <molotov.dmitry@gmail.com>
2023-07-07 11:24:03 +0300

    Added installation of kernel headers when installing DKMS on Debian.

--- tools/afterbuild.sh
+++ tools/afterbuild.sh
@@ -5,6 +5,11 @@ cd "${ROOT_PATH}" || exit 1
 
 . "${ROOT_PATH}/functions.sh"
 
+if [[ "$(lsb_release -si)" == "Debian" ]] && ispkginstalled dkms
+then
+    DEBIAN_FRONTEND=noninteractive silent 'Installing Linux kernel headers' apt install --yes --force-yes --allow-downgrades --allow-remove-essential -qq 'linux-headers-generic'
+fi
+
 DEBIAN_FRONTEND=noninteractive silent 'Remove unnecessary packages' apt autoremove --yes --force-yes --allow-downgrades --allow-remove-essential --purge -qq
 DEBIAN_FRONTEND=noninteractive silent 'Cleaning up'                 apt autoclean
 
