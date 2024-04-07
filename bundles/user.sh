35ff6ed3f2ee3680acd15224d48637a08349b53c
Dmitry Sorokin <molotov.dmitry@gmail.com>
2024-04-07 14:42:05 +0300

    WIP: Added Zorin OS support.

--- bundles/user.sh
+++ bundles/user.sh
@@ -180,7 +180,7 @@ case "${bundle}" in
 
     ## Gnome shell extensions ==================================================
 
-    if ispkginstalled gnome-shell
+    if ispkginstalled gnome-shell && [[ "$(lsb_release -si)" != 'Zorin' ]]
     then
         ## Enable app indicators -----------------------------------------------
 
@@ -606,7 +606,7 @@ case "${bundle}" in
     gtk_theme='Adwaita'
     wm_theme='Adwaita'
 
-    if ispkginstalled gnome-shell
+    if ispkginstalled gnome-shell && [[ "$(lsb_release -si)" != 'Zorin' ]]
     then
         gsettings set org.gnome.desktop.interface icon-theme    "${icon_theme}"
         gsettings set org.gnome.desktop.interface gtk-theme     "${gtk_theme}"
@@ -628,7 +628,7 @@ case "${bundle}" in
     font_fixed_size='12'
     font_table_size='10'
 
-    if ispkginstalled gnome-shell
+    if ispkginstalled gnome-shell && [[ "$(lsb_release -si)" != 'Zorin' ]]
     then
         gsettings set org.gnome.desktop.interface font-name             "${font_ui} ${font_ui_size}"
         gsettings set org.gnome.desktop.interface document-font-name    "${font_doc} ${font_doc_size}"
@@ -665,7 +665,10 @@ case "${bundle}" in
 
     ## Libreoffice  ------------------------------------------------------------
 
-    usercopy 'libreoffice'
+    if [[ "$(lsb_release -si)" != 'Zorin' ]]
+    then
+        usercopy 'libreoffice'
+    fi
 
 ;;
 
