7eb9a9e05880b81eb97505b133e810f39886b7b2
Dmitry Sorokin <molotov.dmitry@gmail.com>
2024-12-04 12:07:19 +0300

    Removed RTL8812AU Wi-Fi driver from work build configuration.

--- custom/tools/work/install.sh
+++ custom/tools/work/install.sh
@@ -11,7 +11,6 @@ appinstall 'Xorriso'                'xorriso'
 
 ### Wi-Fi driver ===============================================================
 
-appinstall 'RTL8812AU driver'       'rtl8812au-dkms'
 appinstall 'RTL8192EU driver'       'rtl8192eu-config rtl8192eu-dkms'
 
 ### Report builder =========================================================
