093bf359f3336eff969839d9c21936ec63db016f
Dmitry Sorokin <molotov.dmitry@gmail.com>
2025-04-04 19:33:15 +0300

    Added system folders symlinking in folders bundle.

--- bundles/firstboot.sh
+++ bundles/firstboot.sh
@@ -441,6 +441,8 @@ case "${bundle}" in
 
 "folders")
 
+    system-folders
+
     if [[ -z "$(grep -v "^#" /etc/fstab | grep /media/documents | grep x-gvfs-hide)" ]]
     then
         sed -i 's/\(\/media\/documents[[:space:]]\+[^[:space:]]\+[[:space:]]\+\)defaults\([[:space:]]\+\)/\1defaults,x-gvfs-hide\2/' /etc/fstab
